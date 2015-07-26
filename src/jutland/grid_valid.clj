69f3a6c2b41f2a71009b47f0d6653e0ef24d118f
Will Hamilton <whamilton42@hotmail.com>
2015-07-27 00:59:19 +0100

    Full grid validation

--- src/jutland/grid_valid.clj
+++ src/jutland/grid_valid.clj
@@ -3,11 +3,88 @@
 (defn- squares [grid]
   (count grid))
 
-(defn- ship-squares [grid]
-  (count (re-seq #"x" grid)))
+(defn- carrier-squares [grid]
+  (count (re-seq #"C" grid)))
+
+(defn- battleship-squares [grid]
+  (count (re-seq #"B" grid)))
+
+(defn- submarine-squares [grid]
+  (count (re-seq #"S" grid)))
+
+(defn- destroyer-squares [grid]
+  (count (re-seq #"D" grid)))
+
+(defn- patrol-boat-squares [grid]
+  (count (re-seq #"P" grid)))
 
 (defn- empty-squares [grid]
   (count (re-seq #" " grid)))
+
+
+(defn- rows [grid]
+  (map clojure.string/join
+    (partition 10 grid)))
+
+(defn columns [grid]
+  (map
+    (fn [column_num]
+      (clojure.string/join
+        (map
+          (fn [row] (nth row column_num))
+          (rows grid)
+        )
+      )
+    )
+    (range 0 10)
+  )
+)
+
+(def ships
+  [
+    {:name "Aircraft Carrier", :symbol "C" :length 5}
+    {:name "Battleship", :symbol "B" :length 4}
+    {:name "Submarine", :symbol "S" :length 3}
+    {:name "Destroyer", :symbol "D" :length 3}
+    {:name "Patrol Boat", :symbol "P" :length 2}
+  ]
+)
+
+(defn- find-ship [key value]
+  (first
+    (filter
+      (fn [ship] (= value (key ship)))
+      ships
+    )
+  )
+)
+
+(defn- ship-regex [ship]
+  (re-pattern (str (:symbol ship) "{" (:length ship) "}"))
+)
+
+(defn ship-in-line? [line ship]
+  (boolean (re-find (ship-regex ship) line))
+)
+
+(defn ship-valid? [grid ship]
+  (boolean
+    (some
+      true?
+      (map
+        (fn [line] (ship-in-line? line ship))
+        (concat (rows grid) (columns grid))
+      )
+    )
+  )
+)
+
+(defn all-ships-valid? [grid]
+  (every?
+    (fn [ship] (ship-valid? grid ship))
+    ships
+  )
+)
 
 
 (defn call [grid]
@@ -15,8 +92,8 @@
     true?
     [
       (= (squares grid) 100)
-      (= (ship-squares grid) 17)
       (= (empty-squares grid) 83)
+      (all-ships-valid? grid)
     ]
   )
 )
--- test/jutland/grid_valid_test.clj
+++ test/jutland/grid_valid_test.clj
@@ -2,28 +2,86 @@
   (:require [clojure.test :refer :all]
             [jutland.grid_valid :refer :all]))
 
+(deftest test-columns
+  (testing "columns"
+    (let [[grid] ["C         C         C         C         C                                                           "]]
+      (is (= (jutland.grid_valid/columns grid) ["CCCCC     " "          " "          " "          " "          " "          " "          " "          " "          " "          "]))
+    )
+  )
+)
+
+(deftest test-ship-in-line?
+  (let [[ship] [{:name "Aircraft Carrier", :symbol "C" :length 5}]]
+    (testing "ship continuous in line"
+      (let [[line] ["CCCCC      "]]
+        (is (= (jutland.grid_valid/ship-in-line? line ship) true))
+      )
+    )
+
+    (testing "ship not non-continuous in line"
+      (let [[line] ["CCC C     "]]
+        (is (= (jutland.grid_valid/ship-in-line? line ship) false))
+      )
+    )
+  )
+)
+
+(deftest test-ship-valid?
+  (let [[ship] [{:name "Aircraft Carrier", :symbol "C" :length 5}]]
+    (testing "ship continuous in row"
+      (let [[grid] [" CCCCC     "]]
+        (is (= (jutland.grid_valid/ship-valid? grid ship) true))
+      )
+    )
+
+    (testing "ship not non-continuous in row"
+      (let [[grid] [" CCCC C    "]]
+        (is (= (jutland.grid_valid/ship-valid? grid ship) false))
+      )
+    )
+
+    (testing "ship continuous in column"
+      (let [[grid] ["C         C         C         C         C         "]]
+        (is (= (jutland.grid_valid/ship-valid? grid ship) true))
+      )
+    )
+
+    (testing "ship not non-continuous in column"
+      (let [[grid] ["C         C         C         C                    C         "]]
+        (is (= (jutland.grid_valid/ship-valid? grid ship) false))
+      )
+    )
+  )
+)
+
 (deftest call_function
   (testing "not enough squares"
-    (let [[grid] ["xxxxx xxxxxxx xxx xx"]]
+    (let [[grid] ["CCCCC BBBB SSS DDD PP"]]
       (is (= (jutland.grid_valid/call grid) false))
     )
   )
 
-  (testing "not enough ship squares"
-    (let [[grid] ["                                                                                xxxxx xxxxxxx xxx   "]]
+  (testing "missing carrier"
+    (let [[grid] ["      BBBB SSS DDD PP"]]
       (is (= (jutland.grid_valid/call grid) false))
     )
   )
 
-  (testing "not correct type of ships"
-    (let [[grid] ["         xxx                                                                    xxxxx xxxx    xxx xx"]]
+  (testing "invalid carrier"
+    (let [[grid] ["CCCC      BBBB SSS DDD PP C"]]
       (is (= (jutland.grid_valid/call grid) false))
     )
   )
 
-  (testing "enough ship squares"
-    (let [[grid] ["                                                                                xxxxx xxxxxxx xxx xx"]]
+  (testing "complex valid grid"
+    (let [[grid] [" D  BBBB   D         DC         C   P     C   P     C  SSS    C                                     "]]
       (is (= (jutland.grid_valid/call grid) true))
+    )
+  )
+
+  (testing "complex slightly invalid grid"
+    (let [[grid] [" D  BBBB   D         DC         C   P     C   P     C   SS    C   S                                 "]]
+      (is (= (jutland.grid_valid/call grid) false))
     )
   )
 )
