8535f9f6f6219af9ddc6bd49b9586de83c1cc863
Will Hamilton <whamilton42@hotmail.com>
2015-07-26 23:40:44 +0100

    WIP grid validation

--- src/jutland/grid_valid.clj
+++ src/jutland/grid_valid.clj
@@ -9,6 +9,60 @@
 (defn- empty-squares [grid]
   (count (re-seq #" " grid)))
 
+(defn rows [grid]
+  (map clojure.string/join
+    (partition 10 grid)
+  )
+)
+
+(defn- columns [grid]
+  (map
+    (fn [column]
+      (map
+        (fn [row]
+          (nth (nth (rows grid) row) column)
+        )
+        (range 0 10)
+      )
+    )
+    (range 0 10)
+  )
+)
+
+(defn line-without-five [line column-num]
+  (let [[preceeding, five, following]
+    [
+      (subs line 0 column-num)
+      (subs line column-num (+ column-num 5))
+      (subs line (+ column-num 5) (min (+ column-num 10) 10))
+    ]]
+
+    (if (= five "xxxxx")
+      (str preceeding "     " following)
+    )
+  )
+)
+
+(defn line-without-fives [line]
+  (filter
+    (complement nil?)
+    (map
+      (fn [column-num] (line-without-five line column-num))
+      (range 0 6)
+    )
+  )
+)
+
+(defn grids-without-five [grid]
+  (flatten
+    (clojure.string/join
+      (map
+        line-without-fives
+        (rows grid)
+      )
+    )
+  )
+)
 
 (defn call [grid]
   (every?
--- test/jutland/grid_valid_test.clj
+++ test/jutland/grid_valid_test.clj
@@ -2,28 +2,96 @@
   (:require [clojure.test :refer :all]
             [jutland.grid_valid :refer :all]))
 
-(deftest call_function
-  (testing "not enough squares"
-    (let [[grid] ["xxxxx xxxxxxx xxx xx"]]
-      (is (= (jutland.grid_valid/call grid) false))
-    )
-  )
+; (deftest test-rows
+;   (testing "full example"
+;     (let [[grid] ["                                                                                xxxxx xxxxxxx xxx xx"]]
+;       (is
+;         (=
+;           [
+;             "          "
+;             "          "
+;             "          "
+;             "          "
+;             "          "
+;             "          "
+;             "          "
+;             "          "
+;             "xxxxx xxxx"
+;             "xxx xxx xx"
+;           ]
+;         (jutland.grid_valid/rows grid))
+;       )
+;     )
+;   )
+; )
 
-  (testing "not enough ship squares"
-    (let [[grid] ["                                                                                xxxxx xxxxxxx xxx   "]]
-      (is (= (jutland.grid_valid/call grid) false))
-    )
-  )
+; (deftest line_without_five
+;   (testing "full example"
+;     (let [[line] ["    xxxxxx"]]
+;       (is (= "         x" (jutland.grid_valid/line-without-five line 4)))
+;       (is (= "    x     " (jutland.grid_valid/line-without-five line 5)))
+;     )
+;   )
+; )
 
-  (testing "not correct type of ships"
-    (let [[grid] ["         xxx                                                                    xxxxx xxxx    xxx xx"]]
-      (is (= (jutland.grid_valid/call grid) false))
-    )
-  )
+; (deftest line_without_fives
+;   (testing "full example"
+;     (let [[line] ["    xxxxxx"]]
+;       (is
+;         (=
+;           [
+;             "         x"
+;             "    x     "
+;           ]
+;         (jutland.grid_valid/line-without-fives line))
+;       )
+;     )
+;   )
+; )
 
-  (testing "enough ship squares"
-    (let [[grid] ["                                                                                xxxxx xxxxxxx xxx xx"]]
-      (is (= (jutland.grid_valid/call grid) true))
+(deftest grids_without_5
+  (testing "full example"
+    (let [[grid] [
+      ; "                                                                                xxxxx xxxxxxx xxx xx"
+      "   xxxxx  "
+      ]]
+      (is
+        (=
+          [
+            "          "
+            ; "                                                                                      xxxxxxx xxx xx"
+            ; "                                                                                xxxxx      xx xxx xx"
+            ; "                                                                                xxxxx x     x xxx xx"
+            ; "                                                                                xxxxx xx      xxx xx"
+          ]
+        (jutland.grid_valid/grids-without-five grid))
+      )
     )
   )
 )
+
+; (deftest call_function
+;   (testing "not enough squares"
+;     (let [[grid] ["xxxxx xxxxxxx xxx xx"]]
+;       (is (= (jutland.grid_valid/call grid) false))
+;     )
+;   )
+
+;   (testing "not enough ship squares"
+;     (let [[grid] ["                                                                                xxxxx xxxxxxx xxx   "]]
+;       (is (= (jutland.grid_valid/call grid) false))
+;     )
+;   )
+
+  ; (testing "5-ship not present"
+  ;   (let [[grid] ["xxxx                                                                            xxxx     xxxx xxx xx"]]
+  ;     (is (= (jutland.grid_valid/call grid) false))
+  ;   )
+  ; )
+
+;   (testing "enough ship squares"
+;     (let [[grid] ["                                                                                xxxxx xxxxxxx xxx xx"]]
+;       (is (= (jutland.grid_valid/call grid) true))
+;     )
+;   )
+; )
